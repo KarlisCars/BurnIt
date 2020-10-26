d57dd9155e939012dcfbba5b5d3c072b83f832bc
Karlis Cars <karlis.cars@gmail.com>
2020-10-26 11:24:45 +0200

    Day6 Of coding. Created a start for my own app. I have to do TableView tomorrow.

---
+++ BurnIt/Controlers/BurnItViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BurnItViewController.swift
+//  BurnIt
+//
+//  Created by Karlis Cars on 26/10/2020.
+//
+
+import UIKit
+
+class BurnItViewController: UIViewController {
+
+
+    
+    @IBOutlet weak var burnitTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func addButton(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BurnIt/Controlers/BurnedTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  BurnedTableViewCell.swift
+//  BurnIt
+//
+//  Created by Karlis Cars on 26/10/2020.
+//
+
+import UIKit
+
+class BurnedTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
